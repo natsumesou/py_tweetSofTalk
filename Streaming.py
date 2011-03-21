8a720fd87c352a68ee167dadf39ede638e0633d9
natsumesou <natsumesouxx@gmail.com>
2011-03-22 02:25:53 +0900

    change reading architecture from subprocess to queue

--- Streaming.py
+++ Streaming.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import os, subprocess, sys, ConfigParser
+import os, subprocess, sys, ConfigParser, time, Queue, threading
 from getpass import getpass
 from tweetSoftalkError import StatusError
 import tweepy
@@ -11,34 +11,41 @@ from pit import Pit
 SofTalk_Path =  '/cygdrive/c/Users/natsumesou/Documents/softalk/SofTalkw.exe'
 SofTalk_Volume = 0
 SofTalk_Speed = 0
+SofTalk_Queue = Queue.Queue(0)
 follow_list = []
 
+tweet_speeds = []
+# calculate tweets speed average
+def speed_average(speed):
+    global tweet_speeds
+    if len(tweet_speeds)>4:
+        tweet_speeds.pop(0)
+        tweet_speeds.append(speed)
+    else:
+        tweet_speeds.append(speed)
 
-def CallSofTalk2(text):
+    tweets = 0
+    for ts in tweet_speeds:
+        tweets += ts
+    return tweets/len(tweet_speeds) if len(tweet_speeds) > 4 else SofTalk_Speed
+
+
+def CallSofTalk(text, speed):
     text = text.strip('\n')
-    options = '\'/X:1 /V:80 /S:180 /W:\"%s\"\'' % (text)
+
+    """
+    global SofTalk_Speed
+    print 'speed:'+str(speed)
+    print 'average:'+str(SofTalk_Speed)
+    """
+
+    options = '\'/X:1 /V:80 /S:%d /W:\"%s\"\'' % (speed, text)
     try:
-        subprocess.Popen([SofTalk_Path, options])
+        return subprocess.Popen([SofTalk_Path, options])
     except OSError, e:
-        print 'OS Error: SofTalk が起動しませんでした'
-        raise
-    except IOError, e:
-        print 'SofTalkw.exe NOT Found: '
-        raise
+        raise OSError, 'OS Error: SofTalk が起動しませんでした: ' + str(e)
 
-def CallSofTalk(text):
-    text = text.encode('utf-8')
-    cmd = SofTalk_Path + ' /X:1 /V:80 /S:180 /W:"%s"' % (text)
-    try:
-        os.system(cmd)
-    except OSError, e:
-        print 'OS Error: SofTalk が起動しませんでした'
-        raise
-    except IOError, e:
-        print 'SofTalkw.exe NOT Found: '
-        raise
-
-
+# checking tweet what is user's TL or NOT
 def check_tweet(status):
     for id in follow_list:
         #checking tweet what is folloing user's or not
@@ -53,12 +60,9 @@ def check_tweet(status):
                 return False
     return False
 
+# around settings.ini
 class Settings():
     CONFIG_FILE = 'settings.ini'
-    username = ''
-    SofTalk_Path = ''
-    SofTalk_Volume = 0
-    SofTalk_Speed = 0
     def __init__(self):
         conf = ConfigParser.SafeConfigParser()
         if not conf.read(self.CONFIG_FILE):
@@ -69,30 +73,94 @@ class Settings():
             self.SofTalk_Path = conf.get('SofTalk', 'path')
             SofTalk_Volume = conf.get('SofTalk', 'volume')
             SofTalk_Speed = conf.get('SofTalk', 'speed')
+            SofTalk_Speed_min = conf.get('SofTalk', 'min_speed')
         except ConfigParser.NoSectionError, e:
             print self.CONFIG_FILE + 'が破損しています:'
             raise
 
-
         try:
             self.SofTalk_Volume = int(SofTalk_Volume)
-        except ValueError, e:
-            print self.CONFIG_FILE+'の [SofTalk] にある volume の値が不正です。: '
-            #sys.exit()
-            raise
+        except ValueError:
+            raise ValueError, self.CONFIG_FILE+'の [SofTalk] にある volume の値が不正です。: ' + SofTalk_Volume
         try:
             self.SofTalk_Speed = int(SofTalk_Speed)
-        except ValueError, e:
-            print self.CONFIG_FILE+'の [SofTalk] にある speed の値が不正です。: '
-            #sys.exit()
-            raise
+        except ValueError:
+            raise ValueError, self.CONFIG_FILE+'の [SofTalk] にある speed の値が不正です。: ' + SofTalk_Speed
+        try:
+            self.SofTalk_Speed_min = int(SofTalk_Speed_min)
+        except ValueError:
+            raise ValueError, self.CONFIG_FILE+'の [SofTalk] にある min_speed の値が不正です。: ' + SofTalk_Speed_min
+
+    # save speed and volume
+    def save(self):
+        global SofTalk_Speed
+        speed = int(SofTalk_Speed)
+        speed = str(speed)
+        conf = ConfigParser.SafeConfigParser()
+        conf.read(self.CONFIG_FILE)
+
+        conf.set('SofTalk', 'speed', speed)
+        f = open(self.CONFIG_FILE, 'w')
+        conf.write(f)
+        f.close()
+
+# calculate tweets speed
+class Check_Speed():
+    global SofTalk_Speed
+    def __init__(self, softalk_speed_min, softalk_speed_max):
+        self.before_time = time.time()
+        self.SofTalk_Speed_min = softalk_speed_min
+        self.SofTalk_Speed_max = softalk_speed_max
+        #self.chars = 0
+    def get_speed(self, chars):
+        now = time.time()
+        before_time = self.before_time
+        self.before_time = now
+        #self.chars += chars
+        speed = chars/(now - before_time)
+        return self.get_adjusted_speed(speed)
+    def get_adjusted_speed(self, speed=None):
+        speed = SofTalk_Speed*speed*0.5 if speed else SofTalk_Speed
+        if speed < self.SofTalk_Speed_min:
+            speed = self.SofTalk_Speed_min
+        if speed > self.SofTalk_Speed_max:
+            speed = self.SofTalk_Speed_max
 
 
+        return speed
+
+def worker():
+    global SofTalk_Speed
+    while True:
+        softalks = SofTalk_Queue.get()
+        sprocess = CallSofTalk(softalks['text'], softalks['speed'])
+        while sprocess.poll() == None:
+            time.sleep(0.5)
+
+# Custion StreamListener
 class SofTalkStreamListener(StreamListener):
+    global SofTalk_Queue
+    def __init__(self, check_speed):
+        StreamListener.__init__(self)
+        thread = threading.Thread(target=worker)
+        thread.setDaemon(True)
+        thread.start()
+        self.queue = SofTalk_Queue
+        self.check_speed = check_speed
+
     def on_status(self, status):
         if check_tweet(status):
+            global SofTalk_Speed
+
             print '[%s] %s\n' % (status.author.screen_name, status.text)
-            CallSofTalk2(status.text)
+            try:
+                speed = self.check_speed.get_speed(len(status.text))
+                SofTalk_Speed = speed_average(speed)
+                self.queue.put({'text': status.text, 'speed':speed})
+                #CallSofTalk(status.text, speed)
+            except Exception, e:
+                print e
+                raise
         else:
             pass
     def on_error(self, status_code):
@@ -104,7 +172,7 @@ class SofTalkStreamListener(StreamListener):
     def on_timeout(self):
         print 'twitter user stream is timeout'
 
-def main():
+def main(settings):
     # for debug
     """
     conf = Pit.get('twitter.com')
@@ -112,7 +180,7 @@ def main():
     passwd = conf['password']
     """
 
-    settings = Settings()
+    # loading settings
     username = settings.username
 
     global SofTalk_Path
@@ -122,7 +190,10 @@ def main():
     SofTalk_Path = settings.SofTalk_Path
     SofTalk_Volume = settings.SofTalk_Volume
     SofTalk_Speed = settings.SofTalk_Speed
+    softalk_speed_min = settings.SofTalk_Speed_min
+    softalk_speed_max = 200
 
+    # insert username and password
     if not username:
         username = raw_input('twitter username: ')
     else:
@@ -131,19 +202,24 @@ def main():
     while not passwd:
         passwd = getpass('password: ')
 
+    # checking user's follow ids
     global follow_list
     follow_list = tweepy.api.friends_ids(screen_name=username)
     follow_list.append(tweepy.api.get_user(username).id)
 
-    stream = Stream(username, passwd, SofTalkStreamListener(), timeout=None)
-    stream.filter(follow=follow_list)
+    print 'connecting to twitter.com ...'
 
-    # add ownself for check_tweet
+    # start twitter UserStream
+    stream = Stream(username, passwd, SofTalkStreamListener(Check_Speed(softalk_speed_min, softalk_speed_max)), timeout=None)
+    stream.filter(follow=follow_list)
 
 if __name__ == "__main__":
     try:
-        main()
+        print '<ctrl-c> End this program'
+        settings = Settings()
+        main(settings)
     except KeyboardInterrupt:
+        settings.save()
         print 'bye'
     except Exception, e:
         print e
--- test.py
+++ test.py
@@ -1,10 +1,63 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from statusError import StatusError
+import sys, subprocess, Queue, time, threading
 
-try:
-    raise StatusError(404)
-except StatusError, e:
-    print e
+SofTalk_Path =  '/cygdrive/c/Users/natsumesou/Documents/softalk/SofTalkw.exe'
 
+def CallSofTalk(text, speed=100):
+    global SofTalk_Path
+    text = text.strip('\n')
+
+    options = '\'/X:1 /V:80 /S:%d /W:\"%s\"\'' % (speed, text)
+    try:
+        print 'speak ' + text
+        #return subprocess.Popen([SofTalk_Path, options])
+        subprocess.call([SofTalk_Path, options])
+    except OSError, e:
+        raise OSError, 'OS Error: SofTalk が起動しませんでした: ' + str(e)
+
+def worker():
+    while True:
+        try:
+            text = queue.get()
+            print 'get queue:' + text
+            CallSofTalk(text)
+            print 'spoke...'
+            """
+            sprocess = CallSofTalk(text)
+            print 'spoke...'
+            while sprocess.poll() == None:
+                print 'sleeping'
+                time.sleep(0.5)
+            """
+        except Exception, e:
+            print type(e)
+            print e
+
+queue = Queue.Queue(0)
+if __name__ == '__main__':
+    try:
+        print 'start'
+        text = 'ほげふば、おなかすいたねん。'
+
+
+        args = ()
+        #t = tthread(target=worker)
+        t = threading.Thread(target=worker)
+        t.setDaemon(True)
+        t.start()
+
+        while True:
+            text = raw_input('input:')
+            if text == 'exit':
+                print 'killed'
+                break
+            print 'put queue'
+            queue.put(text)
+
+    except KeyboardInterrupt:
+        print 'end'
+    except Exception, e:
+        print type(e)
+        print e
